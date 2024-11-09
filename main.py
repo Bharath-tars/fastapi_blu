b078fbba4bda470620ed8d6a4ef33ff056a2dea1
Sudarsanam Bharath <bharath.sudarsanam04@gmail.com>
2024-11-09 17:46:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import time
 
 app = FastAPI()
 
-API_KEY = "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"
+API_KEY = "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"
 subscription_key = "9VyengLPTp5sLNQQms00PWUkAjUI7rZKX2p1UmPJspRkPxQ07DANJQQJ99AKACqBBLyXJ3w3AAAFACOGPbfr"
 endpoint = "https://my-ocr-image.cognitiveservices.azure.com/"
 computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
